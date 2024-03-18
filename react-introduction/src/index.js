e3a9ad41b33ddedc1d122a6547e902401bdd26e6
Cocar Ciprian <Cocar Ciprian>
2024-03-18 18:16:49 +0200

    tot

--- react-introduction/src/index.js
+++ react-introduction/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Products from "./components/Products/Products";
+import PropsExample from "./components/common/PropsExample/PropsExample";
+import Navbar from "./components/navbar/Navbar";
 import "./index.css";
-import ReactIntro from "./ReactIntro";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ReactIntro />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Navbar />
+        <PropsExample name1="Andrei" name2="Mihai" isStudent={true} />
+        <Products />
+    </React.StrictMode>
 );
